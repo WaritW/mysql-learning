91be7b01b98663d42838e75250e8fb0f5343f741
WaritW <Kaofang25tt@gmail.com>
2024-04-24 16:48:49 +0700

    [Added]: เพิ่มการเขียนคำสั่งวันที่ 2

--- mysql-day2/day2.sql
+++ mysql-day2/day2.sql
@@ -19,6 +19,58 @@ WHERE product_name LIKE '_ร%' ;
 SELECT * FROM Products
 WHERE product_name LIKE 'ป%ด' ;
 
+#การใช้คำสั่ง OREDER BY DESC เพื่อเรียกดูข้อมูล ราคาสินค้าจากมากไปน้อย
+SELECT * FROM Products
+ORDER BY price DESC;
 
+#การใช้คำสั่ง OREDER BY ASC เพื่อเรียกดูข้อมูล ราคาสินค้าจากน้อยไปมาก
+SELECT * FROM Products
+ORDER BY in_stocks ASC;
 
+#การใช้คำสั่ง OREDER BY ASC เพื่อเรียกดูข้อมูล ชื่อสินค้าที่ขึ้นต้นด้วย ก-ฮ
+SELECT * FROM Products
+ORDER BY product_name ASC;
 
+#การใช้คำสั่ง OREDER BY DESC เพื่อเรียกดูข้อมูล ชื่อสินค้าที่ขึ้นต้นด้วย ฮ-ก
+SELECT * FROM Products
+ORDER BY product_name DESC;
+
+#การใช้คำสั่ง LIMIT เพื่อเรียกดูข้อมูลตามจำนวนที่ต้องการ
+SELECT product_name, price
+FROM Products
+WHERE price < 100
+LIMIT 2;
+
+#การใช้คำสั่ง UPDATE เพื่ออัพเดทข้อมูลที่มีอยู่ในตาราง
+UPDATE Products
+SET in_stocks = 359
+WHERE product_name = 'ปากกาไวท์บอร์ด';
+
+#การใช้คำสั่ง DELETE ลบข้อมูลออกจากตาราง
+DELETE FROM Products
+WHERE in_stocks = 0;
+
+#การใช้คำสั่ง SUM เพื่อรวมผลลัพธ์
+SELECT SUM(in_stocks) AS sum_stocks
+FROM Products;
+
+#การใช้คำสั่ง COUNT เพื่อนับจำนวนข้อมูล
+SELECT COUNT(product_name) AS count_products
+FROM Products
+
+#การใช้คำสั่ง AVG เพื่อหาค่าเฉลี่ย
+SELECT AVG(price) AS avg_price
+FROM Products;
+
+#การใช้คำสั่ง GROUP BY มักจะใช้ร่วมกับคำสั่ง COUNT(), MAX(), MIN(), SUM(), AVG() เพื่อรวมกลุ่มผลลัพธ์
+SELECT COUNT(product_name), price
+FROM Products
+GROUP BY price;
+
+#การใช้คำสั่ง MIN เพื่อหาค่าน้อยสุด
+SELECT MIN(price)
+FROM Products;
+
+#การใช้คำสั่ง MAX เพื่อหาค่ามากสุด
+SELECT MAX(in_stocks)
+FROM Products;
